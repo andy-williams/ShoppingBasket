504573b1d3880050d306f8cc016a2352b39b8dae
andy-williams <williams.andrew.lirio@gmail.com>
2018-07-01 15:22:39 +0100

    free milk

--- src/ShoppingBasket/DiscountService.cs
+++ src/ShoppingBasket/DiscountService.cs
@@ -9,17 +9,42 @@ namespace ShoppingBasket
         public IList<LineItem> GetDiscountItems(IList<LineItem> items)
         {
             var discounts = new List<LineItem>();
+            discounts.AddRange(GetBreadDiscounts(items));
+            discounts.AddRange(GetFreeMilkOffer(items));
+
+            return discounts;
+        }
+
+        private IList<LineItem> GetFreeMilkOffer(IList<LineItem> items)
+        {
+            var discounts = new List<LineItem>();
+
+            var milkCount = items.Count(x => x.Name == "Milk");
+            var freeMilkAvailable = milkCount > 0 ? milkCount / 4 : 0;
+
+            var milkPrice = items.FirstOrDefault(x => x.Name == "Milk")?.Price;
+            for (var i = 0; i < freeMilkAvailable; i++)
+            {
+                discounts.Add(new LineItem("4xButter - 1 FREE", -(milkPrice.Value)));
+            }
+
+            return discounts;
+        }
+
+        private IList<LineItem> GetBreadDiscounts(IList<LineItem> items)
+        {
+            var discounts = new List<LineItem>();
 
             var butterCount = items.Count(x => x.Name == "Butter");
             var breadDiscountsAvailable = butterCount > 0 ? butterCount / 2 : 0;
             var breadCount = items.Count(x => x.Name == "Bread");
 
-            var breadDiscounts = breadCount > breadDiscountsAvailable
+            var breadDiscountCount = breadCount > breadDiscountsAvailable
                 ? breadDiscountsAvailable
                 : breadCount;
 
             var breadPrice = items.FirstOrDefault(x => x.Name == "Bread")?.Price;
-            for (var i = 0; i < breadDiscounts; i++)
+            for (var i = 0; i < breadDiscountCount; i++)
             {
                 discounts.Add(new LineItem("2xButter - Bread 50% Off", -(breadPrice.Value * 0.5m)));
             }
